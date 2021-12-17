81910e3fb736c4961b4a0953e4ee20f6a28204ac
xinkun cao <xk.cao@trip.com>
2021-12-18 04:32:26 +0800

    [fix]

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,66 +1,173 @@
+const sidebar = {
+    guide: [
+        {
+            title: '基础',
+            collapsable: false,
+            children: [
+                '/guide/introduction',
+                '/guide/installation',
+            ]
+        },
+    ]
+}
+
 module.exports = {
-    title: 'horse 框架体系',
+    title: 'horse 架构',
     description: '',
     head: [
         [
-          'link',
-          {
-            href:
-              'https://fonts.googleapis.com/css?family=Inter:300,400,500,600|Open+Sans:400,600;display=swap',
-            rel: 'stylesheet'
-          }
+            'link',
+            {
+                href:
+                    'https://fonts.googleapis.com/css?family=Inter:300,400,500,600|Open+Sans:400,600;display=swap',
+                rel: 'stylesheet'
+            }
         ],
         [
-          'link',
-          {
-            href:
-              'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-            rel: 'stylesheet'
-          }
+            'link',
+            {
+                href:
+                    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+                rel: 'stylesheet'
+            }
         ],
         [
-          'link',
-          {
-            rel: 'icon',
-            href: '/logo.png'
-          }
+            'link',
+            {
+                rel: 'icon',
+                href: '/logo.png'
+            }
         ],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
         [
-          'meta',
-          { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+            'meta',
+            { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
         ],
-        [
-          'link',
-          {
-            rel: 'apple-touch-icon',
-            href: '/images/icons/apple-icon-152x152.png'
-          }
-        ],
-        [
-          'meta',
-          {
-            name: 'msapplication-TileImage',
-            content: '/images/icons/ms-icon-144x144.png'
-          }
-        ],
+
         ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
         // localization for china
         [
-          'script',
-          {
-            src: 'https://player.youku.com/iframeapi'
-          }
+            'script',
+            {
+                src: 'https://player.youku.com/iframeapi'
+            }
         ]
-      ],
-      themeConfig: {
+    ],
+    themeConfig: {
         nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Guide', link: '/guide/' },
-          { text: 'gitee', link: 'https://google.com' },
+            {
+                text: '文档',
+                ariaLabel: '文档菜单',
+                items: [
+                    {
+                        text: '教程',
+                        link: '/guide/introduction'
+                    },
+                   
+                ]
+            },
+            {
+                text: '常用技术网站',
+                ariaLabel: '常用技术网站',
+                items: [
+                    {
+                        text: 'vue官网',
+                        link: 'https://v3.cn.vuejs.org/'
+                    },
+                    {
+                        text: 'react官网',
+                        link: 'https://react.docschina.org/'
+                    },
+                    {
+                        text: 'springboot官网',
+                        link: 'https://spring.io/projects/spring-boot'
+                    },
+                ]
+            },
+            {
+                text: '常用ui框架',
+                ariaLabel: '常用ui框架',
+                items: [
+                    {
+                        text: 'antd官网',
+                        link: 'https://ant.design/index-cn'
+                    },
+                    {
+                        text: 'element ui',
+                        link: 'https://element-plus.gitee.io/zh-CN/'
+                    },
+                    {
+                        text: 'antd-mobile',
+                        link: 'https://mobile.ant.design/zh'
+                    },
+                ]
+            },
+        ],
+        repo: 'SmileCorporation/horse-docs',
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '最近更新时间',
+        docsDir: 'src',
+        sidebarDepth: 2,
+        sidebar: {
+            collapsable: false,
+            '/guide/': sidebar.guide,
+        },
+
+        smoothScroll: false,
+        carbonAds: {
+            carbon: 'CEBDT27Y',
+            custom: 'CKYD62QM',
+            placement: 'vuejsorg'
+        },
+        topBanner: false
+    },
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer(timestamp) {
+                    const date = new Date(timestamp)
+
+                    const digits = [
+                        date.getUTCFullYear(),
+                        date.getUTCMonth() + 1,
+                        date.getUTCDate(),
+                        date.getUTCHours(),
+                        date.getUTCMinutes(),
+                        date.getUTCSeconds()
+                    ].map(num => String(num).padStart(2, '0'))
+
+                    return '{0}-{1}-{2}, {3}:{4}:{5} UTC'.replace(
+                        /{(\d)}/g,
+                        (_, num) => digits[num]
+                    )
+                }
+            }
+        ],
+        [
+            '@vuepress/pwa',
+            {
+                serviceWorker: true,
+                updatePopup: {
+                    '/': {
+                        message: '内容有更新',
+                        buttonText: '刷新'
+                    }
+                }
+            }
+        ],
+        [
+            'vuepress-plugin-container',
+            {
+                type: 'info',
+                before: info =>
+                    `<div class="custom-block info"><p class="custom-block-title">${info}</p>`,
+                after: '</div>'
+            }
         ]
-      }
-  }
+    ],
+}
--- src/README.md
+++ src/README.md
@@ -1,1 +1,37 @@
-# 进行数据测试
+---
+home: true
+heroText: horse架构
+tagline: 
+actionButtons:
+  - text: Why Vue.js?
+    link: /
+    extraClass: vuemastery-trigger primary
+    icon: fa fa-play-circle
+  - text: 起步
+    link: /guide/introduction
+  - text: GitHub
+    link: https://github.com/vuejs/vue-next
+    extraClass: github grey
+    icon: fa fa-github
+    target: _blank
+features:
+  - title: 易用
+    details: 基于现有框架vue,react,flutter
+  - title: 灵活
+    details: 接口插拔自由，部署方便
+  - title: 高效
+    details: |
+      一套架构，多端适用
+footer: |
+  遵循<a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener"> MIT 开源协议</a><br>
+  Copyright © 2014-2021 Evan You
+socialIcons:
+  - type: GitHub
+    link: https://github.com/vuejs/vue-next
+  - type: Twitter
+    link: https://twitter.com/vuejs
+  - type: Medium
+    link: https://medium.com/the-vue-point
+---
+
+<common-vuemastery-video-modal/>
